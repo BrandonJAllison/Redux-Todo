929f94e1c80fb57106280ba5edcbd04e86965806
Brandon Allison <brandona8128@outlook.com>
2019-04-08 14:39:16 -0400

    added create store to index.js

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
 import { Provider  } from 'react-redux'
+
+const store = createStore(reducer);
 
 ReactDOM.render(<Provider> <App /> </Provider>, document.getElementById('root'));
 
