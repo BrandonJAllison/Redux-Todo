dc7497bea5a05a044b5000bedcb958087b36afed
Brandon Allison <brandona8128@outlook.com>
2019-04-08 15:36:43 -0400

    created TOGGLE_TODO action

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,6 +1,7 @@
 
 
 export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
 
 export const addTodo = todoItem => {
   console.log(todoItem);
@@ -9,3 +10,14 @@ export const addTodo = todoItem => {
     payload: todoItem
   };
 };
+
+
+
+export const toggleToDo = id => {
+    console.log(id);
+    return {
+        type: TOGGLE_TODO,
+        payload: id
+    }
+}
+
